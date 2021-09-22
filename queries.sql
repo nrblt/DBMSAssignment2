066fc6ab07ab951decd76e2444f1ed8933a73dc4
nrblt <44442477+nrblt@users.noreply.github.com>
2021-09-22 15:48:30 +0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,61 +1,73 @@
 create table Driver(
-	driver_id serial,
+	driver_id serial primary key,
 	driver_fname varchar,
 	driver_lname varchar
 );
 
 create table Ambulance_service(
-	ambulance_id serial,
-	ambulance_name varchar
+	ambulance_id serial primary key,
+	ambulance_name varchar,
+	driver_id int references(Driver),
+	hospital_id int references(Hospitals)
+	
 );
 
 create table Hospitals(
-	hospital_id serial,
+	hospital_id serial primary key,
 	hospital_name varchar,
 	hospital_address varchar,
 	town varchar,
 	hospital_status varchar
 );
 
-create table Hospital_medicaments(
-	medicament_id serial,
+create table Medical_treatments(
+	medicament_id serial primary key,
 	medicament_name varchar
 );
 
-create table Medical_treatments(); 
+create table Hospital_medicaments(
+	hospital_id int references(Hospitals),
+	medicament_id int references(Medical_treatments)
+); 
 
 create table Specialization(
-	spec_id serial,
+	spec_id serial primary key,
 	spec_name varchar
 );
 
 create table Doctor(
-	dc_id serial,
-	dc_fname varchar,
+	dc_id serial primary key,
+	dc_fname varchar ,
 	dc_lname varchar,
 	dc_gender varchar, 
-	dc_bday date 
+	dc_bday date,
+	spec_id int references(Specialization),
+	hospital_id int references(Hospitals)
 );
 
 create table Patient(
-	ptn_id serial,
+	ptn_id serial primary key,
 	ptn_fname varchar,
 	ptn_lname varchar, 
 	ptn_gender varchar
 );
 
 create table Bill(
-	bill_id serial,
+	bill_id serial primary key,
 	bill_price int
 );
 
 
 create table Disease (
-	disease_id serial,
+	disease_id serial primary key,
 	diseasename varchar
 ); 	
 
 create table Appointment (
-	appointment_id serial,
-	appointment_date date
+	appointment_id serial primary key,
+	appointment_date date,
+	doctor_id int references(Doctor),
+	patient_id int references(Patient),
+	disease_id int references(Disease),
+	bill_id int references(Bill)
 );
